5cb37e2215051c2e403b34db4b110dcd46f2e807
Axel Petersson <axel.818@gmail.com>
2024-02-12 09:40:11 +0100

    new commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("How old is your dog? And I will tell you how old it is in human years.");
+var dogYear = Console.ReadLine();
+
