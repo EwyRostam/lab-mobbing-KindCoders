96b82fdd3f0a23062fc8c92fb5f2e0a4f0ae52ee
Axel Petersson <axel.818@gmail.com>
2024-02-12 09:50:57 +0100

    added humanYear variable

--- Program.cs
+++ Program.cs
@@ -5,4 +5,6 @@ var dogYear = Console.ReadLine();
 Console.WriteLine($"Your dog would be {int.Parse(dogYear) * 7} years old if he/she was a human");
 
 Console.WriteLine("How old are you and I will give your age in dog years");
+var humanYear = Console.ReadLine();
+
 
