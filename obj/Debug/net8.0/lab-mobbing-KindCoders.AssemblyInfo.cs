b86a4acd883a619e19377799068e309871964568
Axel Petersson <axel.818@gmail.com>
2024-02-12 09:37:24 +0100

    will add var dogYear

--- obj/Debug/net8.0/lab-mobbing-KindCoders.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-mobbing-KindCoders.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-mobbing-KindCoders")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2be5a6ff6b64346b81adfdaf8e03d1b2d7c463da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef00b3cea6755e2c04e8a668aac22591fb5da41d")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-mobbing-KindCoders")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-mobbing-KindCoders")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
